feaf501057ff0849859d5c2f74900fbf1f06a66a
renxiaoshuai <shuai_0303@163.com>
2019-03-20 15:28:17 +0800

    商家系统登录与安全控制

--- pinyougou-parent/pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
+++ pinyougou-parent/pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
@@ -3,12 +3,16 @@ package com.pinyougou.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.omg.CORBA.PRIVATE_MEMBER;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.pinyougou.pojo.TbSeller;
+import com.pinyougou.sellergoods.service.SellerService;
 
 /**
  * 认证类
@@ -17,15 +21,41 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
  */
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+	private SellerService sellerService;
 	
+	public SellerService getSellerService() {
+		return sellerService;
+	}
+
+	public void setSellerService(SellerService sellerService) {
+		this.sellerService = sellerService;
+	}
+
 	@Override
 	public UserDetails loadUserByUsername(String username)
 			throws UsernameNotFoundException {
+		
+
 		System.out.println("经过了这个类型方法阿斯顿发");
 		//构建角色列表
-		List<GrantedAuthority> grantAuths =new ArrayList<GrantedAuthority>();
+		List<GrantedAuthority> grantAuths =new ArrayList();
 		grantAuths.add(new SimpleGrantedAuthority("ROLE_SELLER"));
-		return new User(username,"123456",grantAuths);
+		
+		TbSeller seller = sellerService.findOne(username);
+		if(seller !=null){
+			if(seller.getStatus().equals("1")){
+				return new User(username,seller.getPassword(),grantAuths);
+
+			}else {
+				return null;
+			}
+
+		}else {
+			return null;
+		}
+		
 	}
+
+
 
 }
